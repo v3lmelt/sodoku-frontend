744f0755c08728b77b6580730211a3012759025e
v3lmelt <143810559+v3lmelt@users.noreply.github.com>
2023-09-27 00:09:10 +0800

    删除无用文件test.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import startPage from "@/views/startPage.vue";
 import selectSudokuPage from "@/views/selectSudokuPage.vue"
-import  test from  "@/views/test.vue"
 import sudokuPage from "@/views/sudokuPage.vue";
-import detailSudoku from "@/views/datailSudoku.vue"
+import detailSudoku from "@/views/NavBar.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,11 +23,6 @@ const router = createRouter({
           meta: {
             title: '选择数独'
           }
-        },
-        {
-          path:'/test',
-          name:'test',
-          component:test,
         },
         {
           path:'/sudokuPage/:sudokuID',
